ae6ade8514b5a19f98c94b31e5a1871f23a5ed2d
BraxB <braxb92@gmail.com>
2021-04-17 21:07:18 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+        # Good README Generator [![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg)](http://creativecommons.org/licenses/by-sa/4.0/)
+
+        ## Description
+
+        As a developer who hates to take the time to write individual readme files for every project, I wanted a way to easily generate consistently high quality README files in a short amount of time.
+        Simply put, it solves the problem of having to write out READMEs.
+        Through this project I got to practice string interpolation and dip my toe into nodejs. I also learned a lot about README files themselves in the process.
+
+        ## Table of Contents
+        <ol>
+            <li>Installation</li>
+            <li>Usage</li>
+            <li>Contributing</li>
+            <li>Tests</li>
+            <li>Questions</li>
+        </ol>
+
+        ## Installation
+
+        1. Download a copy of the full package to your local machine
+
+        ## Usage
+
+        1. Open your console and navigate to the root level of the package, 2. Run the command "node index.js", 3. Answer the prompts as thoroughly as you an in regards to your project, 4. Reap the rewards of your new README
+
+        ## Contributing
+
+        Me
+
+        ## Tests
+
+        No tests
+
+        ## Questions
+
+        Braxb92@gmail.com
+
+        ## License
+        Good README Generator is licensed under Attribution-ShareAlike 4.0 International
+
+        ---
+
---
+++ index.js
@@ -0,0 +1,146 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    'What is the name of this project?',
+    'What was your motivation?',
+    'What problem does it solve?',
+    'What did you learn?',
+    'What are the steps to install your project?',
+    'How do you use your project?',
+    'Who would you like to credit?',
+    'Please provide test instructions',
+    'What is your email address?',
+    'What license does this fall under?'
+];
+
+function renderLicenseBadge(license) {
+    if (license === "MIT") {
+      licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    }
+    else if (license === 'Mozilla Publice License 2.0') {
+      licenseBadge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+    }
+    else if (license === 'Attribution-ShareAlike 4.0 International') {
+      licenseBadge = '[![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg)](http://creativecommons.org/licenses/by-sa/4.0/)';
+    }
+    else {licenseBadge = ''};
+    return licenseBadge;
+  }
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(`${fileName}.md`, data);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt([
+        {
+            type:'input',
+            message: questions[0],
+            name: 'projectName',
+        },
+        {
+            type:'input',
+            message: questions[1],
+            name: 'motivation',
+        },
+        {
+            type:'input',
+            message:questions[2],
+            name:'solves'
+        },
+        {
+            type:'input',
+            message:questions[3],
+            name: 'learned'
+        },
+        {
+            type:'input',
+            message: questions[4],
+            name: 'steps'
+        },
+        {
+            type:'input',
+            message: questions[5],
+            name: 'usage'
+        },
+        {
+            type:'input',
+            message: questions[6],
+            name: 'credits'
+        },
+        {
+            type:'input',
+            message: questions[7],
+            name: 'tests'
+        },
+        {
+            type:'input',
+            message: questions[8],
+            name: 'email'
+        },
+        {
+            type:'list',
+            message: questions[8],
+            name: 'license',
+            choices: ['MIT', 'Mozilla Publice License 2.0', 'Attribution-ShareAlike 4.0 International', 'none'],
+            default: 'Attribution-ShareAlike 4.0 International'
+        }
+    ])
+    .then(answers => {
+        let licenseBadge = renderLicenseBadge(answers.license)
+        let fileName = `README`;
+        let data = `
+        
+        # ${answers.projectName} ${licenseBadge}
+        
+        ## Description
+
+        ${answers.motivation}
+        ${answers.solves}
+        ${answers.learned}
+        
+        ## Table of Contents
+        <ol>
+            <li>Installation</li>
+            <li>Usage</li>
+            <li>Contributing</li>
+            <li>Tests</li>
+            <li>Questions</li>
+        </ol>
+
+        ## Installation
+        
+        ${answers.steps}
+        
+        ## Usage
+        
+        ${answers.usage}
+        
+        ## Contributing
+
+        ${answers.credits}
+
+        ## Tests
+
+        ${answers.tests}
+        
+        ## Questions
+
+        ${answers.email}
+
+        ## License
+        ${answers.projectName} is licensed under ${answers.license}
+
+        ---
+        `;
+        writeToFile(fileName, data);
+    })
+}
+
+// Function call to initialize app
+init();
